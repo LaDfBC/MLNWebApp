8d6660dbf2e56ded281dde6b291e1c89924f7628
LaDfBC <george.mausshardt@gmail.com>
2019-05-20 22:52:05 -0500

    Working calculator and translation of stats

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -28,16 +28,35 @@ export class CalculatorComponent implements OnInit {
     this.playerService.getAll().then(data => {
         this.playerMap = data;
         this.pitcherMap = data;
+        this.selectedPlayer = this.playerMap[0];
+        this.selectedPitcher = this.pitcherMap[0];
       }
     );
   }
 
   getDiff(swing: string, pitch: string): void {
-    this.calculatorService.getDiff(swing, pitch)
+    this.calculatorService.getDiff(swing,
+                                   pitch,
+                                   this.buildStatString(this.selectedPlayer, false),
+                                   this.buildStatString(this.selectedPitcher, true))
       .then(value => {
-        const split_value = value.split(',');
-        this.diff = split_value[2];
-        this.result = split_value[3];
+        const split_value = value.split('|');
+        console.log(value);
+        this.diff = split_value[0];
+        this.result = split_value[1];
       });
+  }
+
+  playerChanged(event): void {
+    console.log(event.target);
+    this.selectedPlayer = event.target.data;
+  }
+
+  buildStatString(player: Player, isPitcher: boolean): string {
+    if (isPitcher) {
+      return player.movement + '|' + player.command + '|' + player.velocity + '|' + player.awareness + '|' + player.hand;
+    }
+
+    return player.contact + '|' + player.eye + '|' + player.power + '|' + player.speed + '|' + player.hand;
   }
 }
--- src/services/calculator.service.ts
+++ src/services/calculator.service.ts
@@ -8,13 +8,13 @@ export class CalculatorService {
 
   constructor(private http: HttpClient) { }
 
-  private calculatorUrl = 'http://mlncalc.ddns.net:5432/mln2';
+  private calculatorUrl = 'http://mlncalc.ddns.net:6543/mln';
 
-  getDiff(swing_param: string, pitch_param: string): Promise<string> {
+  getDiff(swing_param: string, pitch_param: string, batter_stats_string: string, pitcher_stats_string: string): Promise<string> {
     return this.http.get(this.calculatorUrl, {
       params: {
-        batter_stats: '5|5|5|5|R',
-        pitcher_stats: '2|2|2|2|L',
+        batter_stats: batter_stats_string,
+        pitcher_stats: pitcher_stats_string,
         swing: swing_param,
         pitch: pitch_param
       },
@@ -22,8 +22,11 @@ export class CalculatorService {
     })
       .toPromise()
       .then(value => {
-        console.log(value);
-        return value.body.toString();
+        console.log(value.body);
+        const body = value.body;
+        const diff = body[2];
+        const result = body[3];
+        return diff + '|' + result;
       });
   }
 }
